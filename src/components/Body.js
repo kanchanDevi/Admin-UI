9c442f079253d65c8fdee0b9ccfbcf8bef008596
kanchanDevi <kanchan.thakurrkr@gmail.com>
2023-02-28 12:39:55 +0530

    completed

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,19 +1,28 @@
 import React, { useState, useEffect } from "react";
 import Header from "./Header";
-import { userData } from "../utils/constant";
+import { USERDATA } from "../utils/constant";
+import { BsFillTrashFill, BsPencilSquare } from "react-icons/bs";
 
 const Body = () => {
   const [search, setSearch] = useState("");
   const [users, setUsers] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
 
   const handlerChange = (e) => {
     setSearch(e.target.value);
   };
 
-
   useEffect(() => {
-    setUsers(userData);
+    response();
   }, []);
+
+  const response = async () => {
+    const data = await fetch(USERDATA);
+    const jsonData = await data.json();
+    setUsers(jsonData);
+    setTotalPages(Math.ceil(jsonData.length / 12));
+  };
 
   const handleChange = (e) => {
     const { name, checked } = e.target;
@@ -22,78 +31,149 @@ const Body = () => {
         return { ...user, isChecked: checked };
       });
       setUsers(tempUser);
-      console.log(tempUser)
+      console.log(tempUser);
     } else {
       let tempUser = users.map((user) =>
         user.name === name ? { ...user, isChecked: checked } : user
       );
       setUsers(tempUser);
-      console.log(tempUser)
+      console.log(tempUser);
     }
   };
+
+  const deleteIcon = (index) => {
+    const newItems = users.filter((user) => {
+      return index !== user.id;
+    });
+    setUsers(newItems);
+    console.log(newItems);
+  };
+
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
+  const handleNextClick = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+  const handlePrevClick = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+  const preDisabled = currentPage === 1;
+  const nextDisabled = currentPage === totalPages;
+
+  const itemsPerPage = 12;
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const itemsToDiaplay = users.slice(startIndex, endIndex);
 
   return (
     <>
       <Header />
-      <div>
+      <div className="p-5 overflow-x-hidden">
         <input
           type="text"
-          className="border border-gray-400 w-[95%]"
+          className="border border-gray-400 w-[95%] h-10 m-5 p-2 "
           value={search}
           onChange={handlerChange}
+          placeholder="search...🔍"
         />
-        <table className="min-w-full text-left text-lg divide-y divide-gray-200">
-          <thead className="bg-gray-50">
-          <tr>
-            <th>
-              <input
-                type="checkbox"
-                name="allSelect"
-                // checked={
-                //   users.filter((user) => user?.isChecked !== true).length < 1
-                // }
-                checked={!users.some((user) => user?.isChecked !== true)}
-                onChange={handleChange}
-              />{" "}
-            </th>
-           
-            <th>Name</th>
-            <th>Email</th>
-            <th>Role</th>
-          </tr>
+        <table className="min-w-[95%] text-left text-lg divide-y p-5 divide-gray-200 m-5">
+          <thead className="bg-gray-50 h-10">
+            <tr>
+              <th>
+                <input
+                  type="checkbox"
+                  name="allSelect"
+                  className="h-4 w-4"
+                  checked={!users.some((user) => user?.isChecked !== true)}
+                  onChange={handleChange}
+                />{" "}
+              </th>
+
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>Actions</th>
+            </tr>
           </thead>
-     
-        {userData
-          .filter((user, index) => {
-            return search.toLocaleLowerCase() === ""
-              ? user
-              : user.name.toLocaleLowerCase().includes(search);
-          })
-          .map((user, index) => {
+
+          {itemsToDiaplay && itemsToDiaplay.length > 0
+            ? itemsToDiaplay
+                .filter((user, index) => {
+                  return search.toLocaleLowerCase() === ""
+                    ? user
+                    : user.name.toLocaleLowerCase().includes(search);
+                })
+                .map((user, index) => (
+                  <tbody key={index} className="h-11">
+                    <tr>
+                      <td>
+                        <input
+                          type="checkbox"
+                          name={user.name}
+                          className="h-5 w-5 "
+                          checked={user?.isChecked || false}
+                          onChange={handleChange}
+                        />
+                      </td>
+                      <td>{user.name}</td>
+                      <td>{user.email}</td>
+                      <td>{user.role}</td>
+                      <td className="flex">
+                        <span className="p-2">
+                          <BsPencilSquare />{" "}
+                        </span>
+                        <span
+                          className="p-2"
+                          onClick={() => {
+                            deleteIcon(user.id);
+                          }}
+                        >
+                          {" "}
+                          <BsFillTrashFill />
+                        </span>
+                      </td>
+                    </tr>
+                  </tbody>
+                ))
+            : ""}
+        </table>
+        <div className=" flex flex-row justify-end m-5 p-5">
+          <button
+            className="p-2 text-black-200 font-bold "
+            onClick={handlePrevClick}
+            disabled={preDisabled}
+          >
+            Prev
+          </button>
+          {Array.from({ length: totalPages }, (_, i) => {
             return (
-              <tbody key={index}>
-                <tr>
-                  <td>  
-                    <input
-                    type="checkbox"
-                      name={user.name}
-                     
-                      checked={user?.isChecked || false}
-                      onChange={handleChange}
-                    />{" "}
-                  </td>
-                  <td>{user.name}</td>
-                  <td>{user.email}</td>
-                  <td>{user.role}</td>
-                </tr>
-              </tbody>
+              <button
+                className="bg-blue-500 rounded-full m-1 w-9 p-2 text-sm text-black-300"
+                onClick={() => handlePageChange(i + 1)}
+                key={i}
+                disabled={i + 1 === currentPage}
+              >
+                {i + 1}
+              </button>
             );
           })}
-          </table>
+
+          <button
+            className="p-2 text-black-200 font-bold"
+            onClick={handleNextClick}
+            disabled={nextDisabled}
+          >
+            Next
+          </button>
+        </div>
       </div>
     </>
   );
-}
-
+};
 
 export default Body;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div>Admin UI</div>
-  )
-}
+    <div className="text-2xl text-black-600 font-extrabold p-5">
+      Admin UI
+    </div>
+  );
+};
 
-export default Header
+export default Header;
